5fc40565536e360bd7771c51ae56d927ef22dab5
FrostMabel <gifari.agif@gmail.com>
2024-03-04 13:44:55 +0700

    First Commit

---
+++ DriverKucing/src/main/java/com/mycompany/driverkucing/DriverKucing.java
@@ -0,0 +1,13 @@
+package com.mycompany.driverkucing;
+
+public class DriverKucing {
+
+    public static void main(String[] args) {
+        Kucing cat0 = new Kucing();
+        cat0.cat1();
+        cat0.cat2();
+        cat0.cat3();
+        cat0.cat4();
+        cat0.cat5();
+    }
+}
---
+++ DriverKucing/src/main/java/com/mycompany/driverkucing/Kucing.java
@@ -0,0 +1,25 @@
+package com.mycompany.driverkucing;
+
+public class Kucing {
+    public String Name = "Arves";
+    public int Umur = 10;
+    public String Jenis = "Himalayan";
+    public String JenisKelamin = "Betina";
+    public String MakananFavorite = "Sardin";
+
+    public void cat1() {
+        System.out.println("Nama: "+ Name);
+    }
+    public void cat2() {
+        System.out.println("Umur: "+ Umur);
+    }
+    public void cat3() {
+        System.out.println("Jenis: "+ Jenis);
+    }
+    public void cat4() {
+        System.out.println("Jenis Kelamin: "+ JenisKelamin);
+    }
+    public void cat5() {
+        System.out.println("MakananFavorite: "+ MakananFavorite);
+    }
+}
